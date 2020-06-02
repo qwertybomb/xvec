eb015aa9ab8fa10db58c6cf91c59e570792b2f3f
qwertybomb <42456119+qwertybomb@users.noreply.github.com>
2020-06-02 16:19:46 -0400

    Add files via upload

--- test.c
+++ test.c
@@ -1,25 +1,27 @@
 #include "xvec.h"
 #include <stdio.h>
 int main() {
-    xvec(int) *v = xvec_new(int);
+    int *v = xvec_new(int);
     xvec_push(v, 1);
     xvec_push(v, 1);
     xvec_push(v, 1, 7, 8, 9, 10, 11);
-    xvec_at(v, 2) = 2;
+    v[2] = 2;
     xvec_insert(v, 0, 12, 3, 5, 6, 7, 8, 9, 10);
     xvec_insert(v, 0, 1, 2, 3, 4);
     xvec_insert(v, 0, 1, 2, 3, 4);
     xvec_insert(v, 0, 1, 2, 3, 4);
     xvec_insert(v, 0, 1, 2, 3, 4);
     xvec_pop(v);
-    xvec_insert(v, v->len, 1, 2, 3, 4);
-    xvec_remove_range(v,v->len-2,v->len-1);
-    xvec_remove(v,v->len-2);
+    xvec_insert(v, xvec_len(v), 1, 2, 3, 4);
+    xvec_remove_range(v,xvec_len(v)-2,xvec_len(v)-1);
+    xvec_remove(v,xvec_len(v)-2);
     xvec_reserve(v,70);
-    printf("%zu\n", v->cap);
-    for (int i = 0; i < v->len; ++i) {
-        printf("%d\n", xvec_at(v, i));
+    xvec_resize(v,3453);
+    xvec_resize(v,32);
+    xvec_pop(v);
+    printf("%zu\n", xvec_cap(v));
+    for (int i = 0; i < xvec_len(v); ++i) {
+        printf("%d\n", v[i]);
     }
-
     xvec_free(v);
 }
--- xvec.h
+++ xvec.h
@@ -1,15 +1,9 @@
-//
-// Created by qwerty on 6/1/20.
-//
-
 #ifndef XVEC_C_XVEC_H
 #define XVEC_C_XVEC_H
+
 #include <stdlib.h>
 #include <string.h>
 
-#define X_NAME(prefix) JOIN(prefix,__LINE__)
-#define JOIN(prefix, postfix) _JOIN(prefix,postfix)
-#define _JOIN(prefix, postfix) prefix##postfix
 
 static inline size_t upper_power_of_two(size_t v) {
     v--;
@@ -27,80 +21,87 @@ static inline size_t upper_power_of_two(size_t v) {
 #define xvec_base struct { size_t cap, len; }
 #define XVEC_ALEN(a) (sizeof(a) / sizeof(*a))
 
-#define xvec(T)               \
-    struct X_NAME(xvec_##T) { \
-        xvec_base;            \
-        T data[];             \
-    }
+static inline size_t *xvec_len_ptr(void *v) {
+    return &((xvec_base *) v - 1)->len;
+}
+static inline size_t *xvec_cap_ptr(void *v) {
+    return &((xvec_base *) v - 1)->cap;
+}
+static inline size_t xvec_len(void* v) {
+    return ((xvec_base *) v - 1)->len;
+}
+static inline size_t xvec_cap(void* v) {
+    return ((xvec_base *) v - 1)->cap;
+}
+
+
 /* how to use
  * T = type
  * ... = items to initialize the array with
  */
-#define xvec_new(T, ...)                                                             \
-({                                                                                   \
-    const T __args[] = {__VA_ARGS__};                                                \
-    const  size_t initial_size = 16+XVEC_ALEN(__args);                               \
-    struct X_NAME(xvec_##T) *v = malloc(sizeof(xvec(T)) + sizeof(T) * initial_size); \
-    v->cap = initial_size;                                                           \
-    v->len = XVEC_ALEN(__args);                                                      \
-    for(size_t i =0; i < v->len; ++i)                                                \
-        v->data[i] = __args[i];                                                      \
-    v;                                                                               \
-})                                                                                   \
+#define xvec_new(T, ...)                                                  \
+({                                                                        \
+    const T __args[] = {__VA_ARGS__};                                     \
+    const  size_t initial_size = 16+XVEC_ALEN(__args);                    \
+    xvec_base *_v = malloc(sizeof(xvec_base) + sizeof(T) * initial_size); \
+    _v->cap = initial_size;                                               \
+    _v->len = XVEC_ALEN(__args);                                          \
+    T* v = (T*)(_v+1);                                                    \
+    for(size_t i =0; i < _v->len; ++i)                                    \
+        v[i] = __args[i];                                                 \
+    v;                                                                    \
+})                                                                        \
 /* how to use
  * v = container
  * size = the size to resize to
  */
-#define xvec_resize(v,size)                                         \
-({                                                                  \
-    const size_t s = size;                                          \
-    /*clear memory before resizing*/                                \
-    if(s > v->len) {                                                \
-        memset(v->data+v->len,0, (v->cap-v->len)*sizeof(*v->data)); \
-    }                                                               \
-    if(s > v->cap) {                                                \
-        v->cap = s;                                                 \
-        v = realloc(v,sizeof(xvec_base) + sizeof(*v->data)*v->cap); \
-    }                                                               \
-    v->len = s;                                                     \
-    v;                                                              \
+#define xvec_resize(v, size)                         \
+({                                                   \
+    const size_t _size = size;                       \
+    size_t* _len = xvec_len_ptr(v);                  \
+    size_t* _cap = xvec_cap_ptr(v);                  \
+    if(_size > *_len) {                              \
+        xvec_reserve(v,_size);                       \
+        /*clear memory after resizing*/              \
+        memset(v+*_len,0, (*_cap-*_len)*sizeof(*v)); \
+        _len = xvec_len_ptr(v);                      \
+    }                                                \
+    *_len = _size;                                   \
+    v;                                               \
 })
 
-#define xvec_reserve(v, size)                                       \
-({                                                                  \
-    const size_t s = size;                                          \
-    if(s > v->cap) {                                                \
-        v->cap = s;                                                 \
-        v = realloc(v,sizeof(xvec_base) + sizeof(*v->data)*v->cap); \
-    }                                                               \
-    v;                                                              \
+#define xvec_reserve(v, size)                                                                  \
+({                                                                                             \
+    const size_t s = size;                                                                     \
+    size_t* _cap = xvec_cap_ptr(v);                                                            \
+    if(s > *_cap) {                                                                            \
+        *_cap = s;                                                                             \
+        v = realloc((xvec_base*)v-1,sizeof(xvec_base) + sizeof(*v)*(*_cap))+sizeof(xvec_base); \
+    }                                                                                          \
+    v;                                                                                         \
 })
 
-#define xvec_free(v) free(v)
+#define xvec_free(v) free((xvec_base*)v-1)
 
-#define xvec_pop(v) ({v->data[--v->len];})
+#define xvec_pop(v) ({v[(*xvec_len_ptr(v))--];})
 
-#define xvec_ref(v, i) (&v->data[i])
-/* how to use
- * v = container
- * i = where
- */
-#define xvec_at(v, i) (*(xvec_ref(v, i)))
 /*how to use
  * v = container
  * ... = items to insert
  */
-#define xvec_push(v, ...)                                          \
-({                                                                 \
-    const __typeof__(*v->data) __args[] = {__VA_ARGS__};           \
-    if(v->len + XVEC_ALEN(__args) >= v->cap) {                     \
-       v->cap = upper_power_of_two(v->len+XVEC_ALEN(__args));      \
-       v = realloc(v,sizeof(xvec_base) + sizeof(*v->data)*v->cap); \
-    }                                                              \
-    for(size_t i = 0; i < XVEC_ALEN(__args); ++i)  {               \
-        v->data[v->len++] = __args[i];                             \
-    }                                                              \
-    v;                                                             \
+#define xvec_push(v, ...)                                           \
+({                                                                  \
+    const __typeof__(*v) __args[] = {__VA_ARGS__};                  \
+    size_t* _len = xvec_len_ptr(v);                                 \
+    size_t* _cap = xvec_cap_ptr(v);                                 \
+    if(*_len + XVEC_ALEN(__args) >= *_cap) {                        \
+       xvec_reserve(v,upper_power_of_two(*_len+XVEC_ALEN(__args))); \
+       _len = xvec_len_ptr(v);                                      \
+    }                                                               \
+    for(size_t i = 0; i < XVEC_ALEN(__args); ++i)  {                \
+        v[(*_len)++] = __args[i];                                   \
+    }                                                               \
+    v;                                                              \
 })
 
 /*how to use
@@ -108,44 +109,48 @@ static inline size_t upper_power_of_two(size_t v) {
  * index = where to insert
  * ... = items to insert
  */
-#define xvec_insert(v, index, ...)                                 \
-({                                                                 \
-    const size_t n = index;                                        \
-    const __typeof__(*v->data) __args[] = {__VA_ARGS__};           \
-    if(v->len + XVEC_ALEN(__args) >= v->cap) {                     \
-       v->cap = upper_power_of_two(v->len+XVEC_ALEN(__args));      \
-       v = realloc(v,sizeof(xvec_base) + sizeof(*v->data)*v->cap); \
-    }                                                              \
-    memcpy(v->data+n+XVEC_ALEN(__args),v->data+n,                  \
-    sizeof(*v->data)*(v->len-n));                                  \
-    memcpy(v->data+n,&__args[0],                                   \
-    XVEC_ALEN(__args)*sizeof(*v->data));                           \
-    v->len+=XVEC_ALEN(__args);                                     \
-    v;                                                             \
+#define xvec_insert(v, index, ...)                                  \
+({                                                                  \
+    const size_t _n = index;                                        \
+    const __typeof__(*v) __args[] = {__VA_ARGS__};                  \
+    size_t* _len = xvec_len_ptr(v);                                 \
+    size_t* _cap = xvec_cap_ptr(v);                                 \
+    if(*_len + XVEC_ALEN(__args) >= *_cap) {                        \
+       xvec_reserve(v,upper_power_of_two(*_len+XVEC_ALEN(__args))); \
+       _len = xvec_len_ptr(v);                                      \
+    }                                                               \
+    memcpy(v+_n+XVEC_ALEN(__args),v+_n,                             \
+    sizeof(*v)*(*_len-_n));                                         \
+    memcpy(v+_n,&__args[0],                                         \
+    XVEC_ALEN(__args)*sizeof(*v));                                  \
+    *_len += XVEC_ALEN(__args);                                     \
+    v;                                                              \
 })
 /* how to use
  * v = container
  * index = where to remove
  */
-#define xvec_remove(v,index)                                                  \
-({                                                                            \
-    const size_t _index = index;                                              \
-    memcpy(v->data+_index,v->data+_index+1,sizeof(*v->data)*(v->len-_index)); \
-    v->len--;                                                                 \
-    v;                                                                        \
+#define xvec_remove(v, index)                              \
+({                                                         \
+    const size_t _index = index;                           \
+    size_t *_len = xvec_len_ptr(v);                        \
+    memcpy(v+_index,v+_index+1,sizeof(*v)*(*_len-_index)); \
+    (*_len)--;                                             \
+    v;                                                     \
 })
 /* how to use
  * v = container
  * first = the first item to be removed
  * last = the last item to be removed
  */
-#define xvec_remove_range(v,first,last)                                       \
-({                                                                            \
-    const size_t _first = first;                                              \
-    const size_t _last = last;                                                \
-    const size_t n = _last-_first+1;                                          \
-    memcpy(v->data+_first,v->data+_last+1,sizeof(*v->data)*(v->len-_last));   \
-    v->len-=n;                                                                \
-    v;                                                                        \
+#define xvec_remove_range(v, first, last)                \
+({                                                       \
+    const size_t _first = first;                         \
+    const size_t _last = last;                           \
+    const size_t _n = _last-_first+1;                    \
+    size_t *_len  = xvec_len_ptr(v);                     \
+    memcpy(v+_first,v+_last+1,sizeof(*v)*(*_len-_last)); \
+    *_len-=_n;                                           \
+    v;                                                   \
 })
 #endif //XVEC_C_XVEC_H
